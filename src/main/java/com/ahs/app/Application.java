2e6d0cd07465ac9687e17a26d188ae9e49b14710
SaranBuddi <47968213+SaranBuddi@users.noreply.github.com>
2024-06-27 10:10:04 +0530

    changes

--- src/main/java/com/ahs/app/Application.java
+++ src/main/java/com/ahs/app/Application.java
@@ -3,7 +3,9 @@ package com.ahs.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan({"com.ahs.controller"})
 @SpringBootApplication
 public class Application {
 
