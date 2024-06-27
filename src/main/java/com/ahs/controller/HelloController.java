a346aafd8b3fcfe16fc50a6f767aca40e1801995
SaranBuddi <47968213+SaranBuddi@users.noreply.github.com>
2024-06-27 09:59:53 +0530

    changes

---
+++ src/main/java/com/ahs/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.ahs.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/app/users")
+public class HelloController {
+
+	@GetMapping("/health")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
